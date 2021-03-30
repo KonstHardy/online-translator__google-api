a3522351b14e33cc42015603c1a5b1df2b3f797c
KonstHardy <konst.hardy@gmail.com>
2021-03-30 16:03:09 +0300

    [add] translation text
    [add] translation text

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 function translation() {
-  console.log("test");
+  let key = "AIzaSyBttL3_rUfMaP8vZQazT8bCd5XhHkmR4lA";
+  let url = "https://translation.googleapis.com/language/translate/v2";
+
+  var content = {
+    q: document.getElementById("lang__from").value,
+    target: document.getElementById("lang__to").value,
+  };
+
+  // console.log("test");
+
+  ajaxRequest(
+    "POST",
+    url + "?key=" + key,
+    translationResponse,
+    JSON.stringify(content)
+  );
+}
+
+function translationResponse() {
+  if (successfulRequest(this)) {
+    var response_json = JSON.parse(this.responseText);
+    var translated_text = "";
+    response_json["data"]["translations"].forEach(function (element) {
+      translated_text += element["translatedText"] + "\n";
+    });
+    document.getElementById("translated").value = translated_text;
+  }
+}
+
+function ajaxRequest(method, url, handlerFunction, content) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open(method, url);
+  xhttp.onreadystatechange = handlerFunction;
+  if (method == "POST") {
+    xhttp.send(content);
+  } else {
+    xhttp.send();
+  }
+}
+
+function successfulRequest(request) {
+  return request.readyState === 4 && request.status == 200;
 }
